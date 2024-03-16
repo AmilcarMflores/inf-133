b02350b85b15b231c1b7848189836ed87b7444bd
AmilcarMflores <amilcarmflores@gmail.com>
2024-03-16 18:36:13 -0400

    servicio_restful

--- tipos_web_services/servicio_restful/server.py
+++ tipos_web_services/servicio_restful/server.py
@@ -17,7 +17,7 @@ class RESTRequestHandler(BaseHTTPRequestHandler):
   # Método para manejar solicitudes GET
   def do_GET(self):
     # Si la ruta es '/lista_estudiantes'
-    if self.path == '/lista_estudiantes':
+    if self.path == '/estudiantes':
       # Se envía una respuesta exitosa (código 200)
       self.send_response(200)
       # Se envía un encabezado indicando que el tipo de contenido es JSON
@@ -25,6 +25,18 @@ class RESTRequestHandler(BaseHTTPRequestHandler):
       self.end_headers()
       # Se envía la lista de estudiantes como respuesta
       self.wfile.write(json.dumps(estudiantes).encode('utf-8'))
+    #Diseño de URL con patrones lógicos y jerarquía
+    elif self.path.startswith("/estudiantes/"):
+      id = int(self.path.split("/")[-1])
+      estudiante = next(
+        (estudiante for estudiante in estudiantes if estudiante["id"] == id),
+        None,
+      )
+      if estudiante:
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(estudiante).encode("utf-8"))
     else:
       # Si la ruta no existe, se envía una respuesta de error (código 404)
       self.send_response(404)
@@ -32,10 +44,12 @@ class RESTRequestHandler(BaseHTTPRequestHandler):
       self.end_headers()
       # Se envía un mensaje de error en formato JSON
       self.wfile.write(json.dumps({"Error": "Ruta no existente"}).encode('utf-8'))
+  
   # Método para manejar solicitudes POST
+  #Uso sustantivos y nombres en plural
   def do_POST(self):
-    # Si la ruta es '/agrega_estudiante'
-    if self.path == '/agrega_estudiante':
+    # Si la ruta es 
+    if self.path == '/estudiantes':
       # Se lee la longitud del contenido del mensaje POST
       content_length = int(self.headers['Content-Length'])
       # Se lee el contenido del mensaje POST y se carga como JSON
@@ -55,6 +69,35 @@ class RESTRequestHandler(BaseHTTPRequestHandler):
       self.send_header('Content-type','application/json')
       self.end_headers()
       self.wfile.write(json.dumps({"Error": "Ruta no existente"}).encode('utf-8'))
+      
+      
+  #Uso correcto de verbos y código de estado HTTP
+  def do_PUT(self):
+    if self.path.startswith("/estudiantes"):
+      content_length = int(self.headers["Content-Length"])
+      data = self.rfile.read(content_length)
+      data = json.loads(data.decode("utf-8"))
+      id = data["id"]
+      estudiante = next(
+        (estudiante for estudiante in estudiantes if estudiante["id"] == id),
+        None,
+      )
+      if estudiante:
+        estudiante.update(data)
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(estudiante).encode("utf-8"))
+  
+  def do_DELETE(self):
+    if self.path == '/estudiantes':
+      self.send_response(200)
+      self.send_header('Content-type', 'application/json')
+      self.end_headers()
+      estudiantes.clear()
+      self.wfile.write(json.dumps(estudiantes).encode('utf-8'))
+      
+  
 # Función para ejecutar el servidor 
 def run_server(port = 8000):
     try:
@@ -71,3 +114,4 @@ def run_server(port = 8000):
 # Bloque principal que ejecuta la función run_server si este archivo es el programa principal
 if __name__ == "__main__":
   run_server()
+  
