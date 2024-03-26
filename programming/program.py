e223a521e15dc6aa7e7f91b563d616a5f9b635e5
AmilcarMflores <161394503+AmilcarMflores@users.noreply.github.com>
2024-03-26 00:35:15 +0000

    Día 3, #100 días de Python

--- programming/program.py
+++ programming/program.py
@@ -3,7 +3,19 @@
 # print(num)
 
 # #Día dos
-a = 12
-b = 2
-resta_dos = a - b
-print(resta_dos)
+# a = 12
+# b = 2
+# resta_dos = a - b
+# print(resta_dos)
+
+#Día tres
+texto_divertido_1 = "(0===0)"
+texto_divertido_2 = "   |   "
+texto_divertido_3 = "[-----]"
+
+print(texto_divertido_1)
+print(texto_divertido_2)
+print(texto_divertido_3)
+
+
+
