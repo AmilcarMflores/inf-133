b725be9baf81e690fc3a615343bbd5f3f76dfbb9
AmilcarMflores <amilcarmflores@gmail.com>
2024-04-24 00:39:34 -0400

    Avance 22/04/2023

---
+++ segundo_parcial/introduction_mvc/app/database.py
@@ -0,0 +1,8 @@
+# Importamos la extensión SQLAlchemy para la gestión de la base de datos.
+# SQLAlchemy proporciona herramientas para trabajar con bases de datos relacionales
+# de manera fácil y eficiente en aplicaciones Flask.
+from flask_sqlalchemy import SQLAlchemy
+
+# Creamos una instancia de SQLAlchemy que será utilizada para interactuar
+# con la base de datos en nuestra aplicación Flask.
+db = SQLAlchemy()
