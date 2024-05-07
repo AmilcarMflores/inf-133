d996be71c45d55dce7898188107b4d50ea4b93be
AmilcarMflores <161394503+AmilcarMflores@users.noreply.github.com>
2024-05-07 11:33:57 +0000

    Avance 06/05/2024

---
+++ segundo_parcial/mvc_api/app/views/animal_view.py
@@ -0,0 +1,23 @@
+# Recibe una lista de animales y formatea 
+def render_animal_list(animals):
+    # Representa una lista de animales como una lista de diccionarios
+    return [
+        {
+            "id": animal.id,
+            "name": animal.name,
+            "species": animal.species,
+            "age": animal.age,
+        }
+        # for de comprensión itera una lista al elemento 
+        for animal in animals
+    ]
+
+# un animal
+def render_animal_detail(animal):
+    # Representa los detalles de un animal como un diccionario
+    return {
+        "id": animal.id,
+        "name": animal.name,
+        "species": animal.species,
+        "age": animal.age,
+    }
