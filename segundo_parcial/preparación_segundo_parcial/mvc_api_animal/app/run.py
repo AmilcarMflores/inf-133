9a32c286c44f40813171fb628a410a673be50233
AmilcarMflores <amilcarmflores@gmail.com>
2024-05-17 15:16:05 -0400

    MVC API Animal

--- segundo_parcial/preparación_segundo_parcial/mvc_api_animal/app/run.py
+++ segundo_parcial/preparación_segundo_parcial/mvc_api_animal/app/run.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from controllers.animal_controller import animal_bp
 from database import db
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
+
+# Configura la url de la documentación OpenAPI
+SWAGGER_URL = "/api/docs"
+API_URL = "/static/swagger.json"
+
+# Inicializa el Blueprint de Swagger UI
+swagger_ui_blueprint = get_swaggerui_blueprint(
+  SWAGGER_URL, API_URL, config={"app_name": "Zoológico API"}
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 # Configuración de la bd
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///zoo.db"
