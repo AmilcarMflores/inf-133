82a4a3bf6a42f4fde67fc6ac445f312cac01e702
AmilcarMflores <amilcarmflores@gmail.com>
2024-05-30 15:44:18 -0400

    Avance 29/05/2024

--- examen_final/api_tdd/app/tests/test_general.py
+++ examen_final/api_tdd/app/tests/test_general.py
@@ -1,14 +1,17 @@
 
+# Prueba para verificar el índice del servidor
 def test_index(test_client):
+    # Enviamos una solicitud GET a la raíz del servidor
     response = test_client.get("/")
-    # Verificar assert(comparación lógica)
+    # Verificamos que el código de estado de la respuesta sea 404 (Not Found)
     assert response.status_code == 404
 
-# verficamos que se levante el swagger:
+# Prueba para verificar que la interfaz de Swagger se levanta correctamente
 def test_swagger_ui(test_client):
-    # verificar --> get
+    # Enviamos una solicitud GET a la ruta de la documentación de Swagger
     response = test_client.get("/api/docs/")
+    # Verificamos que el código de estado de la respuesta sea 200 (OK)
     assert response.status_code == 200
-    # verficamos que la página sí sea swagger:
-    # la cadena "id=swagger-ui" esté dentro de data
+    # Verificamos que el contenido de la página incluya la cadena "id=swagger-ui"
+    # Esto asegura que la página cargada es efectivamente la interfaz de Swagger
     assert b'id="swagger-ui"' in response.data
