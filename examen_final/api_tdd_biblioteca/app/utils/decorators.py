d7126be2403eed371a6d8a6fd72b995fc564c708
AmilcarMflores <amilcarmflores@gmail.com>
2024-06-04 22:41:28 -0400

    Avance 29/05/24

--- examen_final/api_tdd_biblioteca/app/utils/decorators.py
+++ examen_final/api_tdd_biblioteca/app/utils/decorators.py
@@ -1,35 +1,36 @@
-from flask import jsonify
-from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
-from functools import wraps
 import json
+from functools import wraps
 
-# El decorador jwt_required protege una función verificando que la solicitud contenga un JWT válido. Si el token es válido, la función decorada se ejecuta normalmente. Si no, se devuelve una respuesta de error con un estado 401 Unauthorized. 
+from flask import jsonify
+from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request
+
+
 def jwt_required(fn):
-  @wraps(fn)
-  def wrapper(*args, **kwargs):
-    try:
-      verify_jwt_in_request()
-      return fn(*args, **kwargs)
-    except Exception as e:
-      return jsonify({"error": str(e)}), 401
-    
-  return wrapper
-# El decorador roles_required protege una función verificando que la solicitud contenga un JWT válido y que el usuario tenga al menos uno de los roles especificados. Si el token es válido y el usuario tiene un rol permitido, la función decorada se ejecuta normalmente. Si no, se devuelve una respuesta de error con un estado 403 Forbidden (si el rol no es autorizado) o 401 Unauthorized (si hay un problema con el token JWT).
-def roles_required(roles=[]):
-  def decorator(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
-      try:
-        verify_jwt_in_request()
-        current_user = get_jwt_identity()
-        user_roles = json.loads(current_user.get("roles", []))
-        if not set(roles).intersection(user_roles):
-          return jsonify({"error": "Acceso no autorizado para este rol"}), 403
-        return fn(*args, **kwargs)
-      except Exception as e:
-        return jsonify({"error": str(e)}), 401
-    
-    return wrapper
-  return decorator  
-  
+        try:
+            verify_jwt_in_request()
+            return fn(*args, **kwargs)
+        except Exception as e:
+            return jsonify({"error": str(e)}), 401
 
+    return wrapper
+
+
+def roles_required(roles=[]):
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            try:
+                verify_jwt_in_request()
+                current_user = get_jwt_identity()
+                user_roles = json.loads(current_user.get("roles", []))
+                if not set(roles).intersection(user_roles):
+                    return jsonify({"error": "Acceso no autorizado para este rol"}), 403
+                return fn(*args, **kwargs)
+            except Exception as e:
+                return jsonify({"error": str(e)}), 401
+
+        return wrapper
+
+    return decorator
